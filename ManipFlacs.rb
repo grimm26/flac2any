9020d5cb80a2616c2d0adda08a0aa2b25a653168
Mark Keisler <grimm26@gmail.com>
2013-12-20 00:09:05 -0600

    get rid of FORMATS, leave the checking for valid format to within the convert_to method.  new logic for getting tags from the flacs done outside of convert_to.  Minor corrections, changed some exception handling.

--- ManipFlacs.rb
+++ ManipFlacs.rb
@@ -5,11 +5,6 @@ end
 
 class ManipFlacs
   attr_accessor :flacs
-  attr_reader :FORMATS
-
-  # Also find totaltracks
-  FLAC_TAGS = ['ARTIST','ALBUM','TITLE','DATE','TRACKNUMBER','GENRE']
-  FORMATS   = ['ogg','m4a','aac','mp3']
 
   def initialize()
     @flacs = []
@@ -19,10 +14,14 @@ class ManipFlacs
     begin
       if File.directory?(dirfile)
         # Add the flacs in this dir to the @flacs array
-        @flacs.concat(Dir.glob("#{dirfile}/*.flac"))
+        #@flacs.concat(Dir.glob("#{dirfile}/*.flac"))
+        Dir.glob("#{dirfile}/*.flac").each do |file|
+          @flacs << { name: file , tags: get_tags(file) }
+        end
       elsif File.file?(dirfile)
         # Add this file to the @flacs array
-        @flacs << dirfile
+        #@flacs << dirfile
+        @flacs << { name: dirfile,  tags: get_tags(dirfile) }
       else
         raise "#{dirfile} is not a valid file or directory"
       end
@@ -34,58 +33,84 @@ class ManipFlacs
   def convert_to(format)
     begin
       flac_cmd = which('flac')
-      @flacs.each do |flac|
-        metaflac = FlacInfo.new(flac)
-        FLAC_TAGS.each do |t|
-          unless metaflac.hastag?(t)
-            raise FlacInfoError, "#{flac} missing tag #{t}"   
-          end
-        end
+      @flacs.sort_by {|e| e[:name]}.each do |h|
+        flac = h[:name]
+        tags = h[:tags]
         STDERR.print "Encoding #{flac} into "
         case format 
         when "ogg"
           oggenc = which('oggenc')
           outfile = flac.sub(/flac$/, 'ogg')
           STDERR.puts "#{outfile}"
-          cmd = %Q!#{oggenc} --quiet -q5 -o "#{outfile}" -d "#{metaflac.tags['DATE']}" -a "#{metaflac.tags['ARTIST']}" -l "#{metaflac.tags['ALBUM']}" -t "#{metaflac.tags['TITLE']}" -N #{metaflac.tags['TRACKNUMBER']} -G "#{metaflac.tags['GENRE']}" -!
+          cmd = %Q!#{oggenc} --quiet -q5 -o "#{outfile}" -d "#{tags[:date]}" -a "#{tags[:artist]}" -l "#{tags[:album]}" -t "#{tags[:title]}" -N #{tags[:track]} -G "#{tags[:genre]}" -!
+          STDERR.puts "#{cmd}"
         when "mp3"
           lame = which('lame')
           outfile = flac.sub(/flac$/, 'mp3')
           STDERR.puts "#{outfile}"
-          cmd = %Q!#{lame} --quiet --noreplaygain -q2 -b 256 --cbr --ty "#{metaflac.tags['DATE']}" --ta "#{metaflac.tags['ARTIST']}" --tl "#{metaflac.tags['ALBUM']}" --tt "#{metaflac.tags['TITLE']}" --tn #{metaflac.tags['TRACKNUMBER']} --tg '#{metaflac.tags['GENRE']}' --id3v2-only  - #{outfile}!
+          cmd = %Q!#{lame} --quiet --noreplaygain -q2 -b 256 --cbr --ty "#{tags[:date]}" --ta "#{tags[:artist]}" --tl "#{tags[:album]}" --tt "#{tags[:title]}" --tn #{tags[:track]} --tg '#{tags[:genre]}' --id3v2-only  - #{outfile}!
         when "m4a", "aac"
           ffmpeg = which('ffmpeg')
           outfile = flac.sub(/flac$/, 'm4a')
           STDERR.puts "#{outfile}"
-          cmd = %Q!#{ffmpeg} -v fatal -i - -c:a libfdk_aac -vbr 4 -metadata title="#{metaflac.tags['TITLE']}" -metadata artist="#{metaflac.tags['ARTIST']}" -metadata date="#{metaflac.tags['DATE']}" -metadata album="#{metaflac.tags['ALBUM']}" -metadata track=#{metaflac.tags['TRACKNUMBER']} -metadata genre="#{metaflac.tags['GENRE']}" #{outfile}!
+          cmd = %Q!#{ffmpeg} -v fatal -i - -c:a libfdk_aac -vbr 4 -metadata title="#{tags[:title]}" -metadata artist="#{tags[:artist]}" -metadata date="#{tags[:date]}" -metadata album="#{tags[:album]}" -metadata track=#{tags[:track]} -metadata genre="#{tags[:genre]}" #{outfile}!
         else
           raise "Unknown format #{format}"
         end
         system %Q!#{flac_cmd} --decode --stdout --totally-silent "#{flac}" | #{cmd}!
       end
+    rescue RuntimeError => e
+      STDERR.puts e.message
+      #STDERR.puts e.backtrace.inspect
     rescue FlacInfoError => e
       STDERR.puts e.message
-      STDERR.puts e.backtrace.inspect
+      #STDERR.puts e.backtrace.inspect
     rescue FlacInfoReadError => e
       STDERR.puts e.message
-      STDERR.puts e.backtrace.inspect
+      #STDERR.puts e.backtrace.inspect
     rescue FlacCmdsError => e
       STDERR.print "Required command not found: "
       STDERR.puts e.message
-      STDERR.puts e.backtrace.inspect
+      #STDERR.puts e.backtrace.inspect
     end
   end
 
+  private
   #   which('ruby') #=> /usr/bin/ruby
   #   Should also check for supported features that we need, especially in ffmpeg
   def which(cmd)
     exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
     ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
-      exts.each { |ext|
+      exts.each do |ext|
         exe = File.join(path, "#{cmd}#{ext}")
         return exe if File.executable? exe
-      }
+      end
     end
     raise FlacCmdsError, %Q!Could not locate "#{cmd}" for conversion.!
   end
+
+  def get_tags(flacfile)
+    tags = Hash.new
+    FlacInfo.new(flacfile).tags.each do |key, value|
+      case key
+      when 'TITLE'
+        tags.merge!(title: value)
+      when 'ALBUM'
+        tags.merge!(album: value)
+      when 'DATE', 'YEAR'
+        tags.merge!(date: value)
+      when 'ARTIST'
+        tags.merge!(artist: value)
+      when 'TRACK','TRACKNUMBER'
+        tags.merge!(track: value)
+      when 'TOTALTRACKS'
+        tags.merge!(totaltracks: value)
+      when 'GENRE'
+        tags.merge!(genre: value)
+      end
+    end
+    return tags
+  end
+
+
 end
--- flac2any.rb
+++ flac2any.rb
@@ -13,23 +13,22 @@ begin
     opts.banner = "Usage: #{$0} [options]"
 
     options[:format] = 'ogg';
-    opts.on("--format FORMAT", "What format/codec to copy the flac to",flacs.FORMATS.to_a) do |format|
-      options['format'] = format
+    opts.on("--format FORMAT", "What format/codec to copy the flac to") do |format|
+      options[:format] = format
     end
   end.parse!
-  #STDERR.puts options.inspect
 
   if ARGV.length < 1
     raise "Please provide a format and a target directory or file."
   end
 rescue Exception => e
   STDERR.puts e.message
-  #STDERR.puts e.backtrace.inspect
+  STDERR.puts e.backtrace.inspect
 end
 
 ARGV.each do |fileordir|
   STDERR.puts "Processing #{fileordir}"
   flacs.add_flacs(fileordir)
 end
-flacs.convert_to(options['format'])
+flacs.convert_to(options[:format])
 
